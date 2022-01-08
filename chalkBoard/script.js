434b2f2f13c0f5a0d6b3fb4a3c77e4aff391b43d
HaziimYassien <56007548+HaziimYassien@users.noreply.github.com>
2022-01-08 06:12:52 +0200

    testing sandbox

--- chalkBoard/script.js
+++ chalkBoard/script.js
@@ -1,4 +1,3 @@
-
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -12,20 +11,20 @@
 
 //   1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
 const poll = {
-  registerNewAnswer: function(){
-prompt('testing')    
+  registerNewAnswer: function () {
+    prompt("testing");
   }
-}
+};
 //         What is your favourite programming language?
 //         0: JavaScript
 //         1: Python
 //         2: Rust
 //         3: C++
 //         (Write option number)
-  
+
 //   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
 // 2. Call this method whenever the user clicks the "Answer poll" button.
-// 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+// 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 // 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
 // HINT: Use many of the tools you learned about in this and the last section 😉
@@ -37,48 +36,48 @@ prompt('testing')
 
 // GOOD LUCK 😀
 
-
-
 const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
   registerNewAnswer() {
     // Get answer
     const answer = Number(
       prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
       )
     );
     console.log(answer);
 
     // Register answer
-    typeof answer === 'number' &&
+    typeof answer === "number" &&
       answer < this.answers.length &&
       this.answers[answer]++;
 
     this.displayResults();
-    this.displayResults('string');
+    this.displayResults("string");
   },
 
-  displayResults(type = 'array') {
-    if (type === 'array') {
+  displayResults(type = "array") {
+    if (type === "array") {
       console.log(this.answers);
-    } else if (type === 'string') {
+    } else if (type === "string") {
       // Poll results are 13, 2, 4, 1
-      console.log(`Poll results are ${this.answers.join(', ')}`);
+      console.log(`Poll results are ${this.answers.join(", ")}`);
     }
-  },
+  }
 };
 
 document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 // [5, 2, 3]
 // [1, 5, 3, 9, 6, 1]
+
+//testing condesandbox
