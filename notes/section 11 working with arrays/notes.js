31b663281049c0bb95b37291b4e133ff76251520
HaziimYassien <haziim.yassien@gmail.com>
2022-06-14 10:13:24 +0200

    k

--- notes/section 11 working with arrays/notes.js
+++ notes/section 11 working with arrays/notes.js
@@ -12,54 +12,33 @@ console.log(arr.slice(0,2))
 //splice removes the chosen parts 
 arr.splice(1/*start location*/,3/*how many objects to delete*/)
 
-//reverse, reverses the array 
+//reverse, reverses the array
 
 //concat, adds two arrays to each other
 
 
 // //143. the new at method :
-
+console.log('so I skipped this part sorry ')
+const arr = [1, 3, 4, 65, 7, 0]
+//so simply if we want to log the first number
+//we can just use the at method and insert the number's location
+console.log(arr.at(0))
 
 // // 144. looping arrays : for Each: 
 
-// const movements = [100,200,300,400,500,600,700,800,900,1000] 
-// movements.forEach(function(movement){
-//    console.log(movement)
-//  })
-//so this function will be executed multiple times 
-//first time where movement is 100
-//then when movement is 200
-//then when movement is 300
-//then 400 and 500 and 600 and 700 and 800 and 900
 
-//so if we made a special if statment inside the forEach function to confirm that 
+//145. forEach with maps and sets :
 
-const movements = [100,200,300,400,500,600,700,800,900,1000] 
-movements.forEach(function(movement){
-if  (movement < 500){
-  console.log(`${movement} is smaller than 500`)
-}else if(movement > 500){
-  console.log(`${movement} is bigger than 500`)
-}
-  console.log(movement)
- })
-//result will be:
-// 100 is smaller than 500
-// 100
-// 200 is smaller than 500
-// 200
-// 300 is smaller than 500
-// 300
-// 400 is smaller than 500
-// 400
-// 500
-// 600 is bigger than 500
-// 600
-// 700 is bigger than 500
-// 700
-// 800 is bigger than 500
-// 800
-// 900 is bigger than 500
-// 900
-// 1000 is bigger than 500
-// 1000
+//so here's a simple map : 
+const currencies = new Map([
+    ['USD', 'United States Dollar'],
+    ['EUR', "Euro"],
+    ['GBP', "Pound Sterling"],
+])
+
+//now we gon loop over them : 
+currencies.forEach(function (value, key, map) {
+    console.log(`${value} and ${key} and ${map}`)
+})
+
+//so how does this work he didn't define the value or the key or the map ?
